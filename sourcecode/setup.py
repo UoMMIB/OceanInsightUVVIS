344ef6804d6998d69e5bd003ddc7a157db31199c
AlexHenderson <alex.henderson@manchester.ac.uk>
2021-10-25 18:52:03 +0100

    Added setup.py for pip install.
    pip install requires a setup.py file.
    No functional changes to the source code.

---
+++ sourcecode/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='OceanInsightUVVIS',
+    version='v2.0.0',
+    packages=['uvvis', 'utilities', 'utilities.jsonutils', 'utilities.pintutils', 'utilities.datetimeutils'],
+    package_dir={'': 'sourcecode'},
+    url='https://github.com/UoMMIB/OceanInsightUVVIS',
+    license='MIT',
+    author='Alex Henderson',
+    author_email='alex.henderson@manchester.ac.uk',
+    description='Text file parser for Ocean Insight UV-VIS files. '
+)
